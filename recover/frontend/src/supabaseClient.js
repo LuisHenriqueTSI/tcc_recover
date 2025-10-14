e81e1e1e1964f28c1257377b0acff8de892e741c
Luis Henrique Fonseca Victoria <luisvictoria.pl028@academico.ifsul.edu.br>
2025-10-13 21:54:01 -0300

    Novo commit corrigido (sem segredos vazados)

--- recover/frontend/src/supabaseClient.js
+++ recover/frontend/src/supabaseClient.js
@@ -1,6 +1,6 @@
 import { createClient } from '@supabase/supabase-js';
 
-const supabaseUrl = 'https://uiegfwnlphfblvzupziu.supabase.co';
-const supabaseAnonKey = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InVpZWdmd25scGhmYmx2enVweml1Iiwicm9sZSI6InNlcnZpY2Vfcm9sZSIsImlhdCI6MTc2MDM1MzQ4NCwiZXhwIjoyMDc1OTI5NDg0fQ.xFS8s7xpoHkJP8f-2uKhPMHOBOQhXJ7qZGUJOosKePU';
+const supabaseUrl = process.env.REACT_APP_SUPABASE_URL;
+const supabaseAnonKey = process.env.REACT_APP_SUPABASE_KEY;
 
 export const supabase = createClient(supabaseUrl, supabaseAnonKey);
